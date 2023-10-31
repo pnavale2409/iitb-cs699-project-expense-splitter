ad6df216d09246b1f936ef23abd32523e8d9923a
viniiit <vinitp84648@gmail.com>
2023-11-01 03:14:18 +0530

    register

--- expense_splitter/login_register/urls.py
+++ expense_splitter/login_register/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('getLogin/', views.getLogin, name="getLogin"),
     path('login/', views.login, name="login"),
-    
+    path('getRegister/', views.getRegister, name="getRegister"),
+    path('register/', views.register, name="register"),
     # path('logout/', views.logout, name="logout"),
     # path('home/', views.home, name="home"),
     # path('secret/', views.secret, name="secret"),
--- expense_splitter/login_register/views.py
+++ expense_splitter/login_register/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User,Group,auth
 from django.contrib.auth.decorators import login_required, permission_required
-
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -24,7 +25,23 @@ def login(request):
         return redirect('/home')       
     else:
         return HttpResponse("Invalid login credentials")
-    
+
+def getRegister(request):
+    return render(request,'register.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Log in the user after registration
+            # login(request, user)
+            return redirect('/login')  # Replace 'home' with the URL name for your home page
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+
 # def home(request):
 #     return render(request,'home.html')
 
